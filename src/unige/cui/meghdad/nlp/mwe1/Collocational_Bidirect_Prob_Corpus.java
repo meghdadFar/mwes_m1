db348c969750cdbc7c288bf35539d06964f7f439
mfa <meghdad.farahmand@unige.ch>
2016-09-16 16:16:55 +0200

    add license headers

--- src/unige/cui/meghdad/nlp/mwe1/Collocational_Bidirect_Prob_Corpus.java
+++ src/unige/cui/meghdad/nlp/mwe1/Collocational_Bidirect_Prob_Corpus.java
@@ -1,3 +1,19 @@
+/* 
+ * Copyright (C) 2016 Meghdad Farahmand<meghdad.farahmand@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package unige.cui.meghdad.nlp.mwe1;
 
 import java.io.BufferedReader;
@@ -67,7 +83,6 @@ public class Collocational_Bidirect_Prob_Corpus {
     /**
      * Method to return p(w1|w2), p(w2|w1), p(w1|syn(w2)) and p(w2|syn(w1)) for
      * posLabeledCandidates.
-     *
      *
      *
      * @param igCase ignore case
